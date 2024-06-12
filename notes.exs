d06ebea975ec5266b2246681e255d688ad600d5e
Dominic Letz <dominic@diode.io>
2024-06-12 17:03:27 +0200

    Added notes

--- notes.exs
+++ notes.exs
@@ -1,3 +1,13 @@
+# June 6th
+addr = "0x853cc395280a389331de3c7ed7c38588c109e762"
+owner_slot = Hash.to_address(51) |> Base16.encode()
+chain = Chains.Moonbeam
+RemoteChain.RPCCache.get_storage_at(chain, addr, owner_slot, 6316617)
+RemoteChain.RPCCache.get_storage_at(chain, addr, owner_slot, "latest")
+
+latest = RemoteChain.RPCCache.resolve_block(chain, "latest")
+RemoteChain.RPCCache.get_last_change(chain, addr, latest)
+
 # May 15th
 CLIENT1: 0xd5ac10782e3b8650f32731ec2a472775d46770a6
 CLIENT2: 0x3c8b8039f1615a45f02b640c643481c2b08a8db1
