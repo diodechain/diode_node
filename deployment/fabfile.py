73a972765cfc150f75304b6e248ac99ded760b58
Dominic Letz <dominic@diode.io>
2024-06-19 17:11:39 +0200

    Keep cookie on update

--- deployment/fabfile.py
+++ deployment/fabfile.py
@@ -6,7 +6,7 @@ import os
 import time
 from collections import defaultdict
 from datetime import datetime
-from fabric.api import env, run, cd, put, get, abort, hide, local, lcd, prefix, hosts, sudo
+from fabric.api import env, run, cd, put, get, abort, hide, local, lcd, prefix, hosts, sudo, parallel
 from fabric.contrib.files import exists
 
 
@@ -26,7 +26,17 @@ def install():
     h = local("sha1sum ../_build/prod/diode_node.tar.gz", capture=True).split()[0]
     if not exists("diode_node.tar.gz") or h != run("sha1sum diode_node.tar.gz").split()[0]:
       put("../_build/prod/diode_node.tar.gz", env.diode)
-    run("tar xzf diode_node.tar.gz")
+
+    if exists("releases/COOKIE"):
+      run("tar xzf diode_node.tar.gz --exclude=releases/COOKIE")
+    else:
+      run("tar xzf diode_node.tar.gz")
+
+@parallel
+def check():
+  with hide('status', 'running'):
+    local("echo | nc -q0  {} 41046 && echo {}=ok".format(env.host, env.host))
+  run("/opt/diode_node/bin/diode_node pid")
 
 def update():
   with cd(env.diode):
