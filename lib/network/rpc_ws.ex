8cd7352dd5d19c4efbd3c211b8ca0d2de6e85839
Dominic Letz <dominic@diode.io>
2024-06-20 12:42:38 +0200

    Support websocket compression

--- lib/network/rpc_ws.ex
+++ lib/network/rpc_ws.ex
@@ -6,7 +6,7 @@ defmodule Network.RpcWs do
   require Logger
 
   def init(req, state) do
-    {:cowboy_websocket, req, state}
+    {:cowboy_websocket, req, state, %{compress: true}}
   end
 
   def websocket_init(state) do
