1b3b2a6d3c1827e5f10874b1ccac2c731dd60abc
Dominic Letz <dominic@diode.io>
2024-06-20 12:19:45 +0200

    notes

--- notes.exs
+++ notes.exs
@@ -1,3 +1,26 @@
+# June 20th
+
+RemoteChain.RPCCache.get_storage(Chains.Diode, "0xaf60faa5cd840b724742f1af116168276112d6a6")
+RemoteChain.RPCCache.get_storage(Chains.Diode, "0xaf60faa5cd840b724742f1af116168276112d6a6")
+
+RemoteChain.RPCCache.get_block_by_number(Chains.Diode, 7440835)
+RemoteChain.RPC.get_block_by_number(Chains.Diode, 7440835)
+
+
+
+pid = :global.whereis_name({RemoteChain.RPCCache, Chains.Diode})
+state = :sys.get_state(pid)
+
+Map.get(state.request_rpc, {"eth_getStorage", ["0xaf60faa5cd840b724742f1af116168276112d6a6", "0x71905f"]})
+
+RemoteChain.RPCCache.get_balance(Chains.Diode, "0xdd2fd4581271e230360230f9337d5c0430bf44c0", 7442951)
+RemoteChain.RPC.get_balance(Chains.Diode, "0xdd2fd4581271e230360230f9337d5c0430bf44c0", 7442951)
+Map.get(:sys.get_state(pid).request_rpc, {"eth_getBalance", ["0xdd2fd4581271e230360230f9337d5c0430bf44c0", "0x719207"]})
+
+:global.whereis_name({RemoteChain.NodeProxy, Chains.Diode})
+
+RemoteChain.RPCCache.rpc(Chains.Diode, "dio_edgev2", ["0xd48f676574626c6f636b68656164657232837192bd"])
+
 # June 6th
 addr = "0x853cc395280a389331de3c7ed7c38588c109e762"
 owner_slot = Hash.to_address(51) |> Base16.encode()
