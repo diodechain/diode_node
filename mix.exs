19218a9767f48e178c88d20fd5310a36d3854f8e
Dominic Letz <dominic@diode.io>
2024-07-26 16:02:21 +0200

    Update dets_plus to deal with snap relocation

--- mix.exs
+++ mix.exs
@@ -81,8 +81,7 @@ defmodule Diode.Mixfile do
 
   defp deps do
     [
-      {:certmagex, github: "dominicletz/certmagex"},
-      {:zerossl, github: "dominicletz/zerossl", override: true, runtime: false},
+      {:certmagex, "~> 1.0"},
       {:debouncer, "~> 0.1"},
       {:dets_plus, "~> 2.0"},
       {:eblake2, "~> 1.0"},
