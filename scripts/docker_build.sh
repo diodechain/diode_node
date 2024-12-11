e247a008ac93e073cd1b26d6e99b9a20ec5e9c21
Dominic Letz <dominic@diode.io>
2024-12-11 21:08:00 +0100

    build script updates

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -4,7 +4,8 @@
 # Licensed under the Diode License, Version 1.1
 set -e
 export DOCKER_BUILDKIT=0
-docker build . -t diode_node -f scripts/Dockerfile
+export PLATFORM=${PLATFORM:-linux/amd64}
+docker build . --platform=${PLATFORM} -t diode_node -f scripts/Dockerfile
 CID=`docker create diode_node`
 mkdir -p _build/prod
 docker cp "$CID:/app/_build/prod/diode_node.tar.gz" _build/prod/
---
+++ snap/build_prereq.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+export CFLAGS="-O3"
+export KERL_CONFIGURE_OPTIONS="--disable-odbc --disable-javac --disable-debug --with-ssl=/usr/local/openssl/ --disable-dynamic-ssl-lib --without-cdv --without-wx"
+export ASDF_DIR=${HOME}/.asdf
+
+if [ ! -d /usr/local/openssl/ ]; then
+    ./scripts/install_openssl.sh
+fi
+
+if [ ! -d ${ASDF_DIR} ]; then
+    git clone https://github.com/asdf-vm/asdf.git ${ASDF_DIR}
+fi
+
+. ${ASDF_DIR}/asdf.sh
+
+if [ ! -d ${ASDF_DIR}/plugins/erlang ]; then
+    asdf plugin add erlang
+fi
+asdf install erlang
+
+if [ ! -d ${ASDF_DIR}/plugins/elixir ]; then
+    asdf plugin add elixir
+fi
+asdf install elixir
+
+export HEX_HTTP_TIMEOUT=120
+mix local.hex --force --if-missing
+mix local.rebar --force --if-missing
